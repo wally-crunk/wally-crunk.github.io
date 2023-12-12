2680209d45d96755f53a91b6a0b4e1c6d1b8bdaa
wally-crunk <tasty.shawarma@example.edu>
2023-12-12 13:37:31 -0500

    updaterrr ~ fix for menu icons .. tnx amor

--- updaterrr.sh
+++ updaterrr.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+echo "must be in correct directory"
+read -p "continue..."
+
+read -p "Enter comment: " comment_words
+
 git add --all
-git commit -m "updaterrr"
+git commit -m "updaterrr ~ $comment_words"
 git push -u origin main
