974a86850ef9efbfb5b3706a8bc979d6cb6450e9
wally-crunk <tasty.shawarma@example.edu>
2024-02-25 11:35:04 -0500

    updaterrr ~ nojekyll and svgs folder

--- updaterrr.sh
+++ updaterrr.sh
@@ -3,8 +3,9 @@
 echo "must be in correct directory"
 read -p "continue..."
 
+echo "verbose mode."
 read -p "Enter comment: " comment_words
 
-git add --all
-git commit -m "updaterrr ~ $comment_words"
-git push -u origin main
+git add --all --verbose
+git commit -m "updaterrr ~ $comment_words" --verbose
+git push -u origin main --verbose
